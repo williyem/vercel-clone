7ba2bd255f3fbc2c91d8b6a5a3184d4492984e4d
williyem <hinnehwilliams@gmail.com>
2025-03-01 23:43:17 +0000

    fix: made header sticky

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -6,7 +6,7 @@ import { NavigationMenuWithNub } from "./navigation-menu";
 
 const NavBar = () => {
   return (
-    <nav className="flex w-full justify-start bg-neutral-950 p-2 text-neutral-200 md:justify-center">
+    <nav className="flex w-full justify-start dark:bg-black sticky top-0 z-30 p-2 text-neutral-200 md:justify-center">
       <NavigationMenuWithNub />
       {/* <Tabs /> */}
     </nav>
